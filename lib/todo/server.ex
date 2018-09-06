ce3c32bbb3fb05409d53cd40595270f1d5d56621
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-09-06 07:43:24 +0400

    First Version

---
+++ lib/todo/server.ex
@@ -0,0 +1,37 @@
+defmodule Todo.Server do
+  use GenServer
+
+  alias Todo.List
+
+  @impl GenServer
+  def init(_) do
+     Todo.List.new()
+  end
+
+  @impl GenServer
+  def handle_call({:get, date},_, state) do
+    {
+      :reply,
+      Todo.List.entries(state, date),
+      state
+    }
+  end
+
+  @impl GenServer
+  def handle_cast({:put, entry},state) do
+    new_state = Todo.add_entry(state, entry)
+    {:noreply, new_state}
+  end
+
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def add_entry(todoServer, entry) do
+    GenServer.cast(todoServer, {:put, entry})
+  end
+
+  def get_enrties(todoServer, date) do
+    GenServer.call(todoServer, {:get, date}, 5000) 
+  end
+end
